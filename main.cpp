e0619fade9c0805626d3486c32044b62edc967cb
tomas votroubek <tomas.votroubek@outlook.com>
2020-09-13 01:51:20 +0200

    change memory order

--- main.cpp
+++ main.cpp
@@ -3,25 +3,28 @@
 #include <ranges>
 #include <vector>
 
-namespace eig = Eigen;
-namespace rgs = std::ranges;
-
 template <typename T>
 auto min_idx(T begin, T end)
 {
+	namespace rgs = std::ranges;
+
 	auto const min_ptr = rgs::min_element(begin, end);
 	return rgs::distance(begin, min_ptr);
 }
 
 auto fictitious(float *const data, long const width, long const height, size_t const iters)
 {
-	eig::Map<eig::MatrixXf> const payoff_c{data, height, width};
-	eig::MatrixXf const payoff_r{payoff_c.transpose()};
+	namespace eig = Eigen;
+	using matrixf = eig::Matrix<float, eig::Dynamic, eig::Dynamic, eig::RowMajor>;
+	using vectorf = eig::RowVectorXf;
 
-	eig::VectorXf picked_r{payoff_r.rows()};
-	eig::VectorXf picked_c{payoff_r.cols()};
-	eig::VectorXf sum_r{payoff_r.rows()};
-	eig::VectorXf sum_c{payoff_r.cols()};
+	eig::Map<matrixf> const payoff_c{data, height, width};
+	matrixf const payoff_r{payoff_c.transpose()};
+
+	vectorf picked_r{payoff_c.rows()};
+	vectorf picked_c{payoff_c.cols()};
+	vectorf sum_r{payoff_c.rows()};
+	vectorf sum_c{payoff_c.cols()};
 
 	picked_r.setZero();
 	picked_c.setZero();
@@ -32,26 +35,23 @@ auto fictitious(float *const data, long const width, long const height, size_t c
 	{
 		auto const idx_r = min_idx(std::begin(sum_r), std::end(sum_r));
 		picked_r(idx_r) += 1;
-		sum_c += payoff_c.col(idx_r);
+		sum_c += payoff_r.row(idx_r);
 		auto const idx_c = min_idx(std::begin(sum_c), std::end(sum_c));
 		picked_c(idx_c) += 1;
-		sum_r -= payoff_r.col(idx_c);
+		sum_r -= payoff_c.row(idx_c);
 	}
 
-	auto strat_r = picked_r /iters;
-	auto strat_c = picked_c /iters;
+	auto strat_r = picked_r / iters;
+	auto strat_c = picked_c / iters;
 
-	
-	auto xx = std::make_tuple(strat_r, strat_c);
-	auto [stra_r, stra_c] = xx;
-	std::cout << eig::Transpose(stra_r) << std::endl;
-	std::cout << eig::Transpose(stra_c) << std::endl;
+	std::cout << strat_r << std::endl;
+	std::cout << strat_c << std::endl;
 }
 
 int main()
 {
 	std::vector<float> nums;
-	long width = 0, height = 0, iters = 100;
+	long width = 0, height = 0, iters = 100000;
 	std::string line;
 	std::ios_base::sync_with_stdio(false);
 	for (; std::getline(std::cin, line) && !std::empty(line); height++)
