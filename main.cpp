79204200d3ca853b5061d91803ed82b440431570
tomas votroubek <tomas.votroubek@outlook.com>
2020-09-13 18:32:47 +0200

    add "some" comments

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 #include <iostream>
 #include <ranges>
 #include <vector>
-
 #include <cxxopts.h>
 
+// Returns the index of the minimum element.
 template <typename T>
 auto min_idx(T begin, T end)
 {
@@ -14,33 +14,37 @@ auto min_idx(T begin, T end)
 	return rgs::distance(begin, min_ptr);
 }
 
+// Implements the fictitious-play algorithm.
 auto play(float *const data, long const width, long const height, size_t const iters)
 {
 	namespace eig = Eigen;
 	using matrixf = eig::Matrix<float, eig::Dynamic, eig::Dynamic, eig::RowMajor>;
 	using vectorf = eig::RowVectorXf;
 
+	// payoff and payoff.transpose for better locality.
 	eig::Map<matrixf> const payoff_c{data, height, width};
 	matrixf const payoff_r{payoff_c.transpose()};
 
 	vectorf picked_r{payoff_c.rows()};
 	vectorf picked_c{payoff_c.cols()};
-	vectorf sum_r{payoff_c.rows()};
+	vectorf sub_r{payoff_c.rows()};
 	vectorf sum_c{payoff_c.cols()};
 
 	picked_r.setZero();
 	picked_c.setZero();
-	sum_r.setZero();
+	sub_r.setZero();
 	sum_c.setZero();
 
+	// sum_c is summed, sub_r is subtracted. Only need min_idx. Less code
+	// -> Smaller icache? (°_°>). idk.
 	for (size_t i{0}; i < iters; i++)
 	{
-		auto const idx_r = min_idx(std::begin(sum_r), std::end(sum_r));
+		auto const idx_r = min_idx(std::begin(sub_r), std::end(sub_r));
 		++picked_r(idx_r);
 		sum_c += payoff_r.row(idx_r);
 		auto const idx_c = min_idx(std::begin(sum_c), std::end(sum_c));
 		++picked_c(idx_c);
-		sum_r -= payoff_c.row(idx_c);
+		sub_r -= payoff_c.row(idx_c);
 	}
 
 	vectorf strat_r = picked_r / iters;
@@ -49,6 +53,7 @@ auto play(float *const data, long const width, long const height, size_t const i
 	return std::make_tuple(strat_r, strat_c);
 }
 
+// Reads in the number of iterations, or provides help.
 size_t read_opts(int argc, char const *argv[])
 {
 	auto constexpr exe_name = "fictitious_play";
@@ -84,6 +89,7 @@ size_t read_opts(int argc, char const *argv[])
 	return iters;
 }
 
+// Reads the payoff matrix from stdin.
 auto read_in(std::istream &in)
 {
 	std::ios_base::sync_with_stdio(false);
